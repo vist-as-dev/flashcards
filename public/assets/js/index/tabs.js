0b1ecf5a82773c5b4ef9df88ae31b8d181258124
Vitaly Stepanenko <vist.as.dev@gmail.com>
2023-03-11 00:09:40 +0200

    FC-2: tabs

---
+++ public/assets/js/index/tabs.js
@@ -0,0 +1,18 @@
+const ids = ['introduction', 'download'];
+
+const tabs = M.Tabs.init(document.querySelector('.tabs'), {
+    onShow: () => {
+        localStorage.setItem('index.tabs.active', ids[tabs.index]);
+    },
+});
+
+function switchDownloadTab() {
+    tabs.select('download')
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const active = localStorage.getItem('index.tabs.active');
+    if (ids.includes(active)) {
+        tabs.select(active);
+    }
+});
