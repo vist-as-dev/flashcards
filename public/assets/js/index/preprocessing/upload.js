0f632c2c7e9f897c8ca49ef0234e38034134ff8f
Vitaly Stepanenko <vist.as.dev@gmail.com>
2023-03-16 23:46:57 +0200

    FC-2: sections

--- public/assets/js/index/preprocessing/upload.js
+++ public/assets/js/index/preprocessing/upload.js
@@ -1,5 +1,5 @@
 const field = document.getElementById('upload_file');
-const maxSize = 250 * 1024;
+const maxSize = 5 * 1024 * 1024;
 
 field.addEventListener('change', (e) => {
     const [file] = e.target.files;
@@ -12,7 +12,7 @@ field.addEventListener('change', (e) => {
         document.getElementById('upload_file_text').classList.add('invalid');
         document
             .getElementById('upload_file_helper')
-            .dataset.error = 'File too big, please select a file less than 250kb.'
+            .dataset.error = 'File too big, please select a file less than 5Mb.'
         ;
 
         return;
@@ -22,7 +22,24 @@ field.addEventListener('change', (e) => {
         const reader = new FileReader();
         reader.readAsText(file, "UTF-8");
         reader.onload = function (evt) {
-            const content = evt.target.result;
+            let content = evt.target.result;
+            if (file.name.endsWith('.fb2')) {
+                let regex = XRegExp('<binary[^>]*>[^<]*<\\/binary>', 'gm');
+                content = XRegExp.replace(content, regex, '\n');
+
+                const matched = content.matchAll(/<section[^>]*?>[^<]*(<title>(?<title>[\s\S]*?)<\/title>)?[^<]*(<subtitle>(?<subtitle>[\s\S]*?)<\/subtitle>)?(?<text>[\s\S]*?)<\/section>/gm);
+                const sections = [...matched].reduce((acc, {groups}) => {
+                    const title = groups.title && groups.title.replaceAll(/<[^>]*>/gm, '').replaceAll(/\s{2,}/gm, ' ');
+                    const subtitle = groups.subtitle && groups.subtitle.replaceAll(/<[^>]*>/gm, '').replaceAll(/\s{2,}/gm, ' ');
+                    const text = groups.text && groups.text.replaceAll(/<[^>]*>/gm, '').replaceAll(/\s{2,}/gm, ' ');
+
+                    return {...acc, [title.trim() + (subtitle ? '. ' + subtitle.trim() : '')]: text.trim()};
+                }, {["All contents of the book"]: content});
+
+
+                console.log(sections);
+            }
+
             const textarea = document.getElementById('source_text');
 
             textarea.value = content;
