2b9a45045b89840ca7a4ee434e49baa277a0ece5
Vitaly Stepanenko <vist.as.dev@gmail.com>
2024-02-05 17:30:59 +0200

    FC-2: next review toast

--- public/assets/js/src/pages/learning/memorization/Word.js
+++ public/assets/js/src/pages/learning/memorization/Word.js
@@ -183,6 +183,11 @@ export class Word {
                 ? this.#statistics.addCompleted()
                 : this.#statistics.addRepeated();
 
+            const {nextReview} = this.#dictionaries[dictionaryId].flashcards[word.original];
+            const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+            const date = new Date(nextReview);
+            M.toast({html: `<ul><li>origin: ${word.original}</li><li>next review: ${months[date.getMonth()]} ${date.getDate()}</li></ul>`});
+
             this.#state.skip();
 
             const input = this.#body.querySelector('[data-component="input"]');
